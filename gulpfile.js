2c37bbb1b2fadd8a04f3a357e2049253bc55b0a9
Ale <akak@hotmail.it>
2017-08-21 20:31:54 +0200

    Improved styling in first two sections....

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,13 @@
-var gulp = require("gulp"),
-	watch = require("gulp-watch");
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task("default", function(){
-	console.log("hey!!!");
-});
 
-gulp.task("html", function(){
-	console.log("html task");
-});
 
-gulp.task("css", function(){
-	console.log("css task");
-});
 
-gulp.task("watch", function(){
 
-	watch("./app/index.html", function(){
-		gulp.start("html");
-	});
 
-	watch("./app/assets/styles/**/*.css", function(){
-		gulp.start("css");
-	});
-});
+
+
+
+
+
